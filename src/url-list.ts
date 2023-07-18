829226c95ca6a98bd33e531932dc997e2f6c6b49
Matheus <matheushbaraldi@gmail.com>
2023-07-17 23:37:50 -0300

    feat: implemented urlList  to check specific products

---
+++ src/url-list.ts
@@ -0,0 +1,13 @@
+export const urlList = [
+  'https://www.kabum.com.br/produto/432715/placa-de-video-rtx-3060-ti-dual-oc-asus-nvidia-geforce-8-gb-gddr6x-dlss-ray-tracing-dual-rtx3060ti-o8gd6x', //258999
+  'https://www.kabum.com.br/produto/468963/placa-de-video-rtx-4060-oc-edition-dual-asus-nvidia-geforce-8gb-gddr6-dlss-ray-tracing-dual-rtx4060-o8g', //259999
+  'https://www.kabum.com.br/produto/164854/placa-de-video-rtx-3060-asus-dual-o12g-v2-nvidia-geforce-12gb-gddr6-lhr-dlss-ray-tracing-dual-rtx3060-o12g-v2', //206999
+  'https://www.kabum.com.br/produto/270812/placa-de-video-gtx-1660-ti-o6g-evo-oc-asus-tuf-gaming-nvidia-geforce-6gb-gddr6-preto-tuf-gtx1660ti-o6g-evo-gaming', //159999
+  'https://www.pichau.com.br/placa-de-video-mancer-rtx-2060-super-heimdall-ss-8gb-gddr6-256-bit-mcr-rtx2060super-hmdllss',
+  'https://www.pichau.com.br/placa-de-video-asus-geforce-rtx-4090-tuf-gaming-24gb-gddr6x-384-bit-tuf-rtx4090-24g-gaming',
+  'https://www.pichau.com.br/placa-de-video-asus-geforce-rtx-4080-tuf-gaming-16gb-gddr6x-256-bits-tuf-rtx4080-16g-gaming',
+  'https://www.pichau.com.br/placa-de-video-asus-geforce-rtx-3050-tuf-gaming-oc-8gb-gddr6-128-bit-tuf-rtx3050-o8g-gaming',
+  'https://www.gkinfostore.com.br/placa-de-video-galax-geforce-rtx-3060-ti-plus-1-click-oc-lhr-8gb-gddr6x-256bits-36ism6md2kcv',
+  'https://www.gkinfostore.com.br/placa-de-video-msi-geforce-rtx-4070-ventus-2x-oc-12gb-gddr6x-192-bit',
+  'https://www.gkinfostore.com.br/placa-de-video-galax-geforce-rtx-4080-16gb-st-1-click-oc-16gb-gddr6x-256-bit-48nzm6md6lts',
+]
